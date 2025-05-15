7ebf9087001ae339ff7ad2f90990ef45201961da
CCmoririn <moririn2831222@gmail.com>
2025-05-15 12:32:17 +0900

    Update app.py to use environment variable PORT

--- app.py
+++ app.py
@@ -382,3 +382,9 @@ def confirm():
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
 
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
