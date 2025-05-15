3ea15cfb47e3338922907c34518e589811a662fc
CCmoririn <moririn2831222@gmail.com>
2025-05-15 21:40:35 +0900

    Fix credentials file path for Google Sheets authentication

--- spreadsheet_manager.py
+++ spreadsheet_manager.py
@@ -1,3 +1,4 @@
+import os
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -6,7 +7,14 @@ def update_spreadsheet(data):
     スプレッドシートに認識結果を記録（常に3行目に追加）
     """
     SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-    creds = Credentials.from_service_account_file("credentials.json", scopes=SCOPES)
+    
+    # 環境変数から認証情報ファイルのパスを取得
+    creds_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+    if not creds_path:
+        raise Exception("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.")
+    
+    # 取得した認証情報ファイルのパスを使って資格情報を作成
+    creds = Credentials.from_service_account_file(creds_path, scopes=SCOPES)
     client = gspread.authorize(creds)
 
     SPREADSHEET_ID = "1YF4mz5kCL0VVXPNwmkq6pCppm5IR6V_5fZbVLh0Ne9M"
@@ -14,3 +22,4 @@ def update_spreadsheet(data):
 
     worksheet.insert_row(data, 3)
     print("スプレッドシートを更新しました:", data)
+
