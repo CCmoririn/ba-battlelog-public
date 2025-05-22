98b09d910d5dd0237447284ec18d440c5573a17d
CCmoririn <moririn2831222@gmail.com>
2025-05-22 18:59:20 +0900

    fix: キャラ名が反対側に出力される不具合の修正

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def preprocess_image(image_path):
         raise Exception("画像が読み込めませんでした。")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5,5), 0)
-    _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.OTSU)
+    _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if contours:
         x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
